ae4b1e424a861569fff7a8467dfebaae27f9b27b
Patrick Burtchaell <patrick@pburtchaell.com>
2014-03-14 19:07:24 -0500

    Move files to new src/ directory

---
+++ src/js/post.js
@@ -0,0 +1,13 @@
+/*!
+ * headroom.js v0.3.11 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * License: MIT
+ */
+
+(function(global) {
+
+  'use strict';
+
+    $("iframe").contents().find("element-selector").css("border-color", "blue");
+
+}(this));
